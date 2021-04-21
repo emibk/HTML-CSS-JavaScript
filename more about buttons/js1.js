187d215ab46a4bb6ae56a5c44b7c3e68f3953ce6
Emilia Chisim <chisimemilia@gmail.com>
2021-04-21 23:50:13 +0300

    Add files via upload

---
+++ more about buttons/js1.js
@@ -0,0 +1,7 @@
+function changeText()
+{
+  var divElement1=document.getElementById("div1");
+  var divElement2=document.getElementById("div2");
+  divElement1.innerHTML="change 1";
+  divElement2.innerHTML="change 2";
+}
