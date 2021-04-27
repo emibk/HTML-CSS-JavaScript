c923a8794fbd6396363742a6bf4fbef2c6187d58
Emilia Chisim <chisimemilia@gmail.com>
2021-04-27 21:09:42 +0300

    Add files via upload

---
+++ Upload Image/js.js
@@ -0,0 +1,7 @@
+function upload()
+{
+ var imgcanvas = document.getElementById("can");
+ var fileinput = document.getElementById("finput");
+ var image = new SimpleImage(fileinput);
+ image.drawTo(imgcanvas);
+}
